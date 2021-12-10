77afa0df77f855a3a36b50bfbeb892fa15e1dba3
simonsonn7825 <78100141+simonsonn7825@users.noreply.github.com>
2021-12-10 13:44:02 -0600

    Noah script changes

---
+++ Assets/Scripts/Programmer Scripts/NoahScripts/States/DeathState.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathState : State
+{
+   
+    public DeathState(Entity mEntity, FiniteStateMachine mStateMachine) : base(mEntity, mStateMachine)
+    {
+        
+    }
+    public override void StateEnter()
+    {
+        base.StateEnter();
+        mEntity.agent.speed = 0;
+        
+    }
+
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+ 
+    public override void StateExit()
+    {
+        base.StateExit();
+    }
+
+   
+}
