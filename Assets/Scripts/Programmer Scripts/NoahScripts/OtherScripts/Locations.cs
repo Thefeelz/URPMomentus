f0a47c9b2564d72356ab3d51a24b299522f894b4
simonsonn7825 <78100141+simonsonn7825@users.noreply.github.com>
2022-02-27 10:19:52 -0600

    Noah Script Changes

--- Assets/Scripts/Programmer Scripts/NoahScripts/OtherScripts/Locations.cs
+++ Assets/Scripts/Programmer Scripts/NoahScripts/OtherScripts/Locations.cs
@@ -10,15 +10,17 @@ public class Locations : MonoBehaviour
     public GameObject lPlayer;
     public Vector3 lPlayerPos;
     public Vector3[] lSpots = new Vector3[8];
-    public bool[] lSpotsTaken = new bool[8];
+    public Enemy_Melee[] lSpotsTaken = new Enemy_Melee[8];
     public bool[] lSpotsValid = new bool[8];
     public GameObject prefab;
     public GameObject[] objects = new GameObject[8];
     public NavMeshAgent agent;
     public NavMeshPath navMeshPath;
+    public Entity[] enemies;
 
     void Start()
     {
+        enemies = FindObjectsOfType(typeof(Enemy_Melee)) as Enemy_Melee[]; // gets all melee enemies
         navMeshPath = new NavMeshPath();
         agent = GetComponent<NavMeshAgent>();
         int l = 0;
@@ -50,7 +52,7 @@ public class Locations : MonoBehaviour
             {
                 if (!(j == 0 && i == 0))
                 {
-                    lSpots[l] = new Vector3(lPlayer.transform.position.x + i, lPlayer.transform.position.y, lPlayer.transform.position.z + j);
+                    lSpots[l] = new Vector3(lPlayer.transform.position.x + i * 2, lPlayer.transform.position.y, lPlayer.transform.position.z + j * 2);
                     objects[l].transform.position = lSpots[l];
                     NavMeshHit hit;
                     if (NavMesh.SamplePosition(lSpots[l], out hit, 0.1f, NavMesh.AllAreas))
@@ -61,12 +63,33 @@ public class Locations : MonoBehaviour
                     {
                         lSpotsValid[l] = false;
                     }
-                        l += 1;
+                    if(lSpotsValid[l] && lSpotsTaken[l] == null)
+                    {
+                        Enemy_Melee closest = null;
+                        float minDist = Mathf.Infinity;
+                        foreach(Enemy_Melee em in enemies)
+                        {
+                            if (em.hasTarget == false && lSpotsTaken[l] == null) // only run if the enemy has no assigned spot
+                            {
+                                float dist = Vector3.Distance(lSpots[l], em.transform.position);
+                                if (dist < minDist)
+                                {
+                                    minDist = dist;
+                                    closest = em;
+                                }
+                                if (closest)
+                                {
+                                    closest.hasTarget = true;
+                                    closest.spot = l;
+                                    lSpotsTaken[l] = closest;
+                                }
+                            }
+
+                        }
+                    }
+                    l += 1;
                 }
             }
         }
-
-
-
     }
 }
