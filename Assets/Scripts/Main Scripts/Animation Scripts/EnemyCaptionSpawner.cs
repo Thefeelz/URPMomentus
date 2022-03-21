6f89cd772aad37572ce2337994db4a6a5400e022
Thefeelz <72534405+Thefeelz@users.noreply.github.com>
2022-03-21 12:04:57 -0500

    Caption changes for enemy

--- Assets/Scripts/Main Scripts/Animation Scripts/EnemyCaptionSpawner.cs
+++ Assets/Scripts/Main Scripts/Animation Scripts/EnemyCaptionSpawner.cs
@@ -6,14 +6,18 @@ public class EnemyCaptionSpawner : MonoBehaviour
 {
     [SerializeField] GameObject takeDamageCaption;
     [SerializeField] Sprite[] spriteRenderers;
-    // Start is called before the first frame update
+    [SerializeField] float timeCaptionIsActive;
+
     public void SpawnDamageCaption()
     {
         if (takeDamageCaption)
         {
-            GameObject newObject = Instantiate(takeDamageCaption, transform.position, Quaternion.identity);
+            GameObject newObject = Instantiate(takeDamageCaption, new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z), Quaternion.identity);
             newObject.GetComponentInChildren<SpriteRenderer>().sprite = spriteRenderers[Random.Range(0, spriteRenderers.Length - 1)];
             newObject.transform.LookAt(Camera.main.transform.position);
+            Destroy(newObject, timeCaptionIsActive);
         }
     }
+
+    public void DestroyCaption() { Destroy(this); }
 }
