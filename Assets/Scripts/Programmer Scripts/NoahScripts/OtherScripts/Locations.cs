9ad304ec7aa3449add64a29b04c9711c41e0e915
simonsonn7825 <78100141+simonsonn7825@users.noreply.github.com>
2022-03-21 10:15:35 -0500

    ai fixes

--- Assets/Scripts/Programmer Scripts/NoahScripts/OtherScripts/Locations.cs
+++ Assets/Scripts/Programmer Scripts/NoahScripts/OtherScripts/Locations.cs
@@ -32,11 +32,18 @@ public class Locations : MonoBehaviour
     {
         transform.position = lPlayer.transform.position;
         spotUpdate();
-        if (Input.GetKeyDown(KeyCode.U))
+        for (int i = 0; i < enemies.Length; i++)
         {
-            foreach (Enemy_Melee em in enemies)
+            for (int j = 0; j < enemies.Length; j++)
             {
-                em.moveState.Calculate();
+                if (i != j)
+                {
+                    if (enemies[i].spot == enemies[j].spot)
+                    {
+                        currentEnemies.Clear();
+                        restartSpots();
+                    }
+                }
             }
         }
     }
@@ -153,28 +160,32 @@ public class Locations : MonoBehaviour
         }
         currentEnemies.Sort((e1, e2) => Vector3.Distance(e1.transform.position, lPlayer.transform.position).CompareTo(Vector3.Distance(e2.transform.position, lPlayer.transform.position))); // sorting algorithm
         Enemy_Melee[] closeEnemies = new Enemy_Melee[4]; // the 4 closest
-        currentEnemies.CopyTo(0, closeEnemies, 0, 3); // copies the the closet for the array
+        currentEnemies.CopyTo(0, closeEnemies, 0, 4); // copies the the closet for the array
         foreach (Enemy_Melee en in enemies)
         {
             if (en == closeEnemies[0])
             {
                 en.hasTarget = true;
                 en.spot = 0;
+                lSpotsTaken[0] = en;
             }
             else if (en == closeEnemies[1])
             {
                 en.hasTarget = true;
                 en.spot = 1;
+                lSpotsTaken[1] = en;
             }
             else if (en == closeEnemies[2])
             {
                 en.hasTarget = true;
                 en.spot = 2;
+                lSpotsTaken[2] = en;
             }
             else if (en == closeEnemies[3])
             {
                 en.hasTarget = true;
                 en.spot = 3;
+                lSpotsTaken[3] = en;
             }
             else
             {
@@ -205,25 +216,83 @@ public class Locations : MonoBehaviour
     public void DeathRelocate(int spot)
     {
 
+        enemies = FindObjectsOfType(typeof(Enemy_Melee)) as Enemy_Melee[]; // gets all melee enemies
         if (spot != 9)
         {
             currentEnemies.Clear();
-            enemies = FindObjectsOfType(typeof(Enemy_Melee)) as Enemy_Melee[]; // gets all melee enemies
+
             for (int i = 0; i < enemies.Length; i++) // The list is just made for efficient sorting
             {
-                if ((enemies[i].spot == 9 || enemies[i].spot == 0))
+                if (enemies[i].spot == 9)
                 {
                     currentEnemies.Add(enemies[i]);
                 }
             }
             currentEnemies.Sort((e1, e2) => Vector3.Distance(e1.transform.position, lPlayer.transform.position).CompareTo(Vector3.Distance(e2.transform.position, lPlayer.transform.position)));
-            if (currentEnemies[0]) 
+            if (currentEnemies[0])
             {
                 Enemy_Melee tempE = currentEnemies[0];
                 tempE.hasTarget = true;
                 tempE.spot = spot;
             }
-            
+
+        }
+        
+    }
+
+    public void restartSpots()
+    {
+        enemies = FindObjectsOfType(typeof(Enemy_Melee)) as Enemy_Melee[];
+        currentEnemies.Clear();
+        for (int i = 0; i < enemies.Length; i++) // The list is just made for efficient sorting
+        {
+            if (enemies[i].hasTarget == false) // only add if enemy does not have a target
+            {
+                currentEnemies.Add(enemies[i]);
+            }
+        }
+        int freeSpot = 0;
+        for(int i = 0; i < 4; i++) // this will get us a number of how many spots are currently free
+        {
+            if(!lSpotsTaken[i])
+            {
+                freeSpot += 1;
+            }
+        }
+        currentEnemies.Sort((e1, e2) => Vector3.Distance(e1.transform.position, lPlayer.transform.position).CompareTo(Vector3.Distance(e2.transform.position, lPlayer.transform.position))); // sorting algorithm
+        //Enemy_Melee[] closeEnemies = new Enemy_Melee[freeSpot]; // the x closest enemies, x being the amount of free spots
+        //currentEnemies.CopyTo(0, closeEnemies, 0, freeSpot); // copies the the closest to the array
+        foreach (Enemy_Melee en in currentEnemies) // assigns enemies by how close they are to available spots
+        {
+            if (lSpotsTaken[0] == null)
+            {
+                en.hasTarget = true;
+                en.spot = 0;
+                lSpotsTaken[0] = en;
+            }
+            else if (lSpotsTaken[1] == null)
+            {
+                en.hasTarget = true;
+                en.spot = 1;
+                lSpotsTaken[1] = en;
+            }
+            else if (lSpotsTaken[2] == null)
+            {
+                en.hasTarget = true;
+                en.spot = 2;
+                lSpotsTaken[2] = en;
+            }
+            else if (lSpotsTaken[3] == null)
+            {
+                en.hasTarget = true;
+                en.spot = 3;
+                lSpotsTaken[3] = en;
+            }
+            else
+            {
+                en.hasTarget = false;
+                en.spot = 9;
+            }
         }
     }
 }
